c2868a543437a2242b3fee42f9339e027cf719e7
Oğuz Yeşil <oguzyesil17@gmail.com>
2022-12-04 14:46:18 +0300

    Update Youtube.py

--- Youtube.py
+++ Youtube.py
@@ -1,37 +1,40 @@
 from googleapiclient.discovery import build
 import random
 
-youtubeApi = "AIzaSyAmXIZ2vp7-hspVf9cAB1OHRBxF5jkyb7I"
-apiServiceName = 'youtube'
-apiVersion = 'v3'
+# YouTube API constants
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+YOUTUBE_API_KEY = "AIzaSyAmXIZ2vp7-hspVf9cAB1OHRBxF5jkyb7I"
 
-prefix = ['VIDEO', 'VIDEO ','VIDEO-', 'VIDEO_', 'IMG ', 'IMG_', 'IMG-', 'DSC ', 'VID']
-postfix = [' MOV', '.MOV', 'MOV', 'AVI', ' AVI', '.AVI', ' MP4', '.MP4', 'MP4']
+# Common prefixes and postfixes for YouTube video titles
+PREFIXES = ['VIDEO', 'VIDEO ', 'VIDEO-', 'VIDEO_', 'IMG ', 'IMG_', 'IMG-', 'DSC ', 'VID']
+POSTFIXES = [' MOV', '.MOV', 'MOV', 'AVI', ' AVI', '.AVI', ' MP4', '.MP4', 'MP4']
 
 def youtube_search():
-    youtube = build(apiServiceName, apiVersion, developerKey=youtubeApi)
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=YOUTUBE_API_KEY)
+    
+    # Build a random query string using one of the common prefixes and postfixes
+    query = random.choice(PREFIXES) + str(random.randint(999, 9999)) + random.choice(POSTFIXES)
     
     search_response = youtube.search().list(
-        q=random.choice(prefix) + str(random.randint(999, 9999)) + random.choice(postfix),
+        q=query,
         part='snippet',
         maxResults=5
-          ).execute()
-
+    ).execute()
+    
     videos = []
-    video = ""
-
+    
+    # Get a list of video IDs from the search results
     for search_result in search_response.get('items', []):
         if search_result['id']['kind'] == 'youtube#video':
             videos.append('%s' % (search_result['id']['videoId']))
     
-    if(len(videos) != 0):
-        
-        return "https://www.youtube.com/watch?v=" + videos[random.randint(0, len(videos)-1)]
-        
-    else:
+    # If no videos were found, try again with a new random query
+    if not videos:
         return youtube_search()
+    
+    # Return a random video from the list of found videos
+    return "https://www.youtube.com/watch?v=" + videos[random.randint(0, len(videos)-1)]
 
+# Print a random YouTube video URL
 print(youtube_search())
-
-
-
